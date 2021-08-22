2364c2a6a9c4407374a5a19dce83795f7e40468b
dibyendu2paul <87035939+dibyendu2paul@users.noreply.github.com>
2021-08-22 21:47:20 +0100

    Commit CRUD Operations in REST API

---
+++ Server/Controllers/EmployeesController.cs
@@ -0,0 +1,107 @@
+﻿using BlazorWebApp.Server.Services;
+using BlazorWebApp.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository employeeRepository;
+
+        public EmployeesController(IEmployeeRepository employeeRepository)
+        {
+            this.employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetEmployees()
+        {
+            try
+            {
+                return Ok(await employeeRepository.GetEmployees());
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrivieving all data from database");
+            }
+
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int Id)
+        {
+            try
+            {
+                var result = await employeeRepository.GetEmployee(Id);
+                if (result == null) return NotFound();
+                return result;
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrivieving data by ID from database");
+            }
+
+        }
+
+        public async Task<ActionResult<Employee>> CreateEmployee(Employee employee)
+        {
+            try
+            {
+                var createdEmployee = await employeeRepository.AddEmployee(employee);
+                if (createdEmployee == null) BadRequest();
+                return CreatedAtAction(nameof(GetEmployee), new { Id = createdEmployee.EmployeeId}, createdEmployee);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error creating new employee");
+            }
+
+        }
+        [HttpPut("{Id:int}")]
+        public async Task<ActionResult<Employee>> UpdateEmployee(int Id, Employee employee)
+        {
+            try
+            {
+                if (Id != employee.EmployeeId) return BadRequest("Employee Id is not matching");
+                var employeeToUpdate = await employeeRepository.GetEmployee(Id);
+                if (employeeToUpdate == null) return NotFound($"Employee with Id = {Id} not found");
+                return await employeeRepository.UpdateEmployee(employee);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error updating employee");
+            }
+
+        }
+
+        [HttpDelete("{Id:int}")]
+        public async Task<ActionResult> DeleteEmployee(int Id)
+        {
+            try
+            {
+                var employeeToDelete = await employeeRepository.GetEmployee(Id);
+                if (employeeToDelete == null) return NotFound($"Employee with Id = {Id} not found");
+                await employeeRepository.DeleteEmployee(Id);
+                return Ok($"Employee with Id = {Id} deleted");
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error deleting employee");
+            }
+
+        }
+
+
+    }
+}
---
+++ Server/Services/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlazorWebApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWebApp.Server.Services
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(int employeeId);
+        Task<Employee> AddEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+        Task DeleteEmployee(int employeeId);
+    }
+}
